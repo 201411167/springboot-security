3dd9bdf3aecd514b6cf33fb5a936486aa4af96d2
김기덕 <rlarlejr103@gmail.com>
2020-01-18 15:56:58 +0900

    간단한 스프링 시큐리티 예제

--- src/main/java/com/example/demo/config/WebSecurityConfig.java
+++ src/main/java/com/example/demo/config/WebSecurityConfig.java
@@ -9,8 +9,12 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
@@ -33,19 +37,22 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .antMatchers("/admin/**").hasRole("ADMIN")
-                .antMatchers("/user/myinfo").hasRole("MEMBER")
-                .antMatchers("/**").permitAll();
+                .antMatchers("/user/**").hasRole("MEMBER")
+                .antMatchers("/","/index","/sign/**").permitAll()
+                .antMatchers("/**").authenticated();
         http.formLogin()
                 //.loginPage( i want to use default login page supported by spring )
-                .defaultSuccessUrl("/user/login/result")
+                .defaultSuccessUrl("/home")
+                .failureUrl("/error")
                 .permitAll();
         http.logout()
-                .logoutRequestMatcher(new AntPathRequestMatcher("/user/logout"))
-                .logoutSuccessUrl("/user/logout/result")
+                //.logoutRequestMatcher(new AntPathRequestMatcher(""))
+                .logoutSuccessUrl("/index")
                 .invalidateHttpSession(true);
-        http.exceptionHandling()
-                .accessDeniedPage("/user/denied");
+        http.csrf()
+                .disable();
     }
+
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,17 +1,36 @@
 package com.example.demo.controller;
 
+import com.example.demo.domain.user.UserEntity;
 import com.example.demo.domain.user.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class UserController {
     @Autowired
     UserService userService;
 
-    @GetMapping("/")
+    @GetMapping({"/","/index"})
     public String index(){
         return "index";
+    }
+
+    @GetMapping("/sign")
+    public String sign(){
+        return "sign";
+    }
+
+    @PostMapping("/sign/result")
+    public String sign_result(@RequestParam(value="id") String id, @RequestParam(value="email") String email, @RequestParam(value="pw") String pw){
+        userService.addUser(UserEntity.builder().id(id).email(email).password(pw).authority("MEMBER").build());
+        return "index";
+    }
+
+    @GetMapping("/home")
+    public String home(){
+        return "home";
     }
 }
